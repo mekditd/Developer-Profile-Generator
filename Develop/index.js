54ce2044d8a55fcf28f8633f6fb23362e708b383
Tedla B <mekdi.td87@outlook.com>
2020-01-17 23:21:06 -0600

    wrote codes in index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,46 @@
 const questions = [
-  
+
 ];
 
 function writeToFile(fileName, data) {
- 
+
 }
 
 function init() {
 
-init();
+    init();
 }
+
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt({
+        message: "Enter your GitHub username",
+        name: "username"
+    })
+
+    
+    .then(function ({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+        axios.get(queryUrl).then(function ({ data }) {
+            const repoNames = data.map(function (repo) {
+                return repo.name;
+            });
+
+            const repoNamesStr = repoNames.join("\n");
+            const filename = "repos.txt";
+
+
+
+            fs.writeFile("repos.txt", repoNamesStr, function (err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Saved ${repoNames.length} repos`);
+            });
+        });
+    });
