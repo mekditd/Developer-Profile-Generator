a7514cd18c8d63dfddb5c1504117560e34fdfecc
Tedla B <mekdi.td87@outlook.com>
2020-01-21 03:47:34 -0600

    color choice added

--- Develop/index.js
+++ Develop/index.js
@@ -17,12 +17,22 @@ const inquirer = require("inquirer");
 const pdfMaker = require("pdfkit");
 
 inquirer
-    .prompt({
-        message: "Enter your GitHub username",
-        name: "username"
-    })
+inquirer.prompt([
+    {
+        type: "input",
+        message: ("What is your Github user name?"),
+        name: "userName",
+    },
 
-    
+    {
+        type: "rawlist",
+        message: ("What is your favorite color?"),
+        choices: ["pink", "blue", "pink","red"],
+        name: "favoriteColor",
+
+    }
+])
+
     .then(function ({ username }) {
         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
         console.log(queryUrl);
@@ -44,4 +54,23 @@ inquirer
                 console.log(`Saved ${repoNames.length} repos`);
             });
         });
+    });
+
+
+const questions = []
+
+
+
+    .then(function (response) {
+        console.log(response);
+        fs.writeFile(response.username + ".JSON",
+            JSON.stringify(response, null, 2),
+            function (err) {
+
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("success!");
+                }
+            });
     });
