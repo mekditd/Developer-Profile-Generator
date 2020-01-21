617d0442fec4e0d2689f13f2f04e4ac5f615ef73
Tedla B <mekdi.td87@outlook.com>
2020-01-21 04:33:25 -0600

    solved conglicts

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a88d9f5a318c80c5d90051657eea2db418758545917aafbec19344fd820936ea
-size 1608
+oid sha256:dd6d9eddacb1f5297962b28f037d48c860f41d74328438e0723fdcb5b3dff931
+size 1650
