fa97c7409bbc4e58a046fa43966e95f61c951840
Tedla B <mekdi.td87@outlook.com>
2020-01-22 09:50:44 -0600

    latest changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
 const questions = [
 
 ];
@@ -11,66 +16,47 @@ function init() {
     init();
 }
 
-const fs = require("fs");
-const axios = require("axios");
-const inquirer = require("inquirer");
+
 // const pdfMaker = require("pdfkit");
 
-inquirer
+
 inquirer.prompt([
     {
         type: "input",
         message: ("What is your Github user name?"),
-        name: "userName",
+        name: "username",
     },
 
-    {
-        type: "rawlist",
-        message: ("What is your favorite color?"),
-        choices: ["pink", "blue", "pink","red"],
-        name: "favoriteColor",
+    // {
+    //     type: "list",
+    //     message: ("What is your favorite color?"),
+    //     choices: ["pink", "blue", "pink", "red"],
+    //     name: "favoriteColor",
 
-    }
+    // }
 ])
 
     .then(function ({ username }) {
         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
         console.log(queryUrl);
         axios.get(queryUrl).then(function ({ data }) {
-            const repoNames = data.map(function (repo) {
-                return repo.name;
+            const repoNames = data.map((repo) => repo.name);
+        })
+
+            .then(function ({ data }) {
+                const follower = data.map((followers) => followers.id);
+                console.log(follower);
+                // });
+
+
+
+
+                fs.writeFile("repos.txt", repoNamesStr, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log()
+                    console.log(`Saved ${repoNames.length} repos`);
+                });
             });
-
-            const repoNamesStr = repoNames.join("\n");
-            const filename = "repos.txt";
-
-
-
-            fs.writeFile("repos.txt", repoNamesStr, function (err) {
-                if (err) {
-                    throw err;
-                }
-
-                console.log(`Saved ${repoNames.length} repos`);
-            });
-        });
-    });
-
-
-// const questions = []
-
-
-
-    // .then(function (response) {
-    //     console.log(response);
-    //     fs.writeFile(response.username + ".JSON",
-    //         JSON.stringify(response, null, 2),
-    //         function (err) {
-
-    //             if (err) {
-    //                 console.log(err);
-    //             } else {
-    //                 console.log("success!");
-    //             }
-    //         });
-    // });
+    })
