2b703d0c805e0bf7fc10379454b4419efabc7a1a
Tedla B <mekdi.td87@outlook.com>
2020-03-07 07:07:28 -0600

    updated version

--- Develop/generateHTML.js
+++ Develop/generateHTML.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:803192949cb9e3a70d49e04fc7abcf0f6722e6223d4491d181ffde66a86252ef
-size 6232
+oid sha256:fab4eec40efe37198431e3b490df5847f61d45c14a1b42a39dccc1fc0634a690
+size 9204
